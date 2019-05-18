5ee295fcbe8447860ea16032583c075eac5ac013
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-05-18 21:22:12 +0300

    Implement selectUnit

--- src/dev/kirillzhelt/registry/jdbcconnections/UnitsSQLiteJDBCConnection.java
+++ src/dev/kirillzhelt/registry/jdbcconnections/UnitsSQLiteJDBCConnection.java
@@ -41,6 +41,21 @@ public class UnitsSQLiteJDBCConnection extends SQLiteJDBCConnection {
     }
 
     public Unit selectUnit(int unitNumber) {
+        String selectUnitSql = "SELECT superior_unit_id, full_name, short_name, name_in_genetive, name_in_dative FROM units WHERE unit_id=?";
+
+        try (PreparedStatement selectUnitStmt = connection.prepareStatement(selectUnitSql)) {
+            selectUnitStmt.setInt(1, unitNumber);
+
+            ResultSet rs = selectUnitStmt.executeQuery();
+
+            if (rs.next())
+                return new Unit(unitNumber, rs.getInt("superior_unit_id"),
+                    rs.getString("full_name"), rs.getString("short_name"),
+                    rs.getString("name_in_genetive"), rs.getString("name_in_dative"));
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
         return null;
     }
 
